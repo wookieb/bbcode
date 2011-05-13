f86840a2a436444aaa80d090c6ebfedb9f3e70e1
Marcin Ławniczak <marcin.safmb@wp.pl>
2011-05-13 07:31:20 -0700

    Błędy ortograficzne i brak znacznika zamykającego

--- tester.php
+++ tester.php
@@ -1,8 +1,10 @@
 <?php
-$news = 'W czwartek 21 kwietnia br. odbylo sie pierwsze spotkanie z rodzicami zawodników  rodzicami zawodników  rodzicami zawo zawodników  rodzicami zawodników z rocznika 2000 uczestniczacych w zajeciach prowadzonych w ramach Akademii Pilkarskiej. W zebraniu uczestniczyli: prezes Klubu Sportowego Górnik Wesola Ryszard Jedyk, przedstawiciel sponsora glównego Przemyslaw Byzdra – Dyrektor Generalny, Dyrektor ds. Rozwoju Akademii Pilkarskiej Mariusz Kaletka i trener koordynator Radoslaw Gabiga. Omówione zostaly sprawy organizacyjne i regulaminowe szkólki oraz planowany program zajec.';
+// To jest test skracania tekstu
+$news = 'W czwartek 21 kwietnia br. odbyło się pierwsze spotkanie z rodzicami zawodników  rodzicami zawodników  rodzicami zawodników  rodzicami zawodników z rocznika 2000 uczestniczących w zajęciach prowadzonych w ramach Akademii Piłkarskiej. W zebraniu uczestniczyli: prezes Klubu Sportowego Górnik Wesoła Ryszard Jedyk, przedstawiciel sponsora głównego Przemyslaw Byzdra – Dyrektor Generalny, Dyrektor ds. Rozwoju Akademii Piłkarskiej Mariusz Kaletka i trener koordynator Radosław Gabiga. Omówione zostały sprawy organizacyjne i regulaminowe szkółki oraz planowany program zajęć.';
 require('Bbcode/BbCode.php');
 $bbcode = new BbCode();
 $bbcode->getSettings()->trustText = true;
 $bbcode->parse($news);
 $cutText = $bbcode->cutText(260); 
 echo mb_strlen($cutText, 'utf-8');
+?>
